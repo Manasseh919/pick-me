e3cb429d3431e5d7b51fcd522d1507b74292575e
Manasseh919 <69004938+Manasseh919@users.noreply.github.com>
2024-09-13 01:24:39 +0200

    layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import localFont from "next/font/local";
 import "./globals.css";
 import { ClerkProvider } from "@clerk/nextjs";
 import {Outfit} from 'next/font/google'
+import NavBar from "@/components/NavBar";
 
 const geistSans = localFont({
   src: "./fonts/GeistVF.woff",
@@ -31,6 +32,7 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <NavBar/>
         {children}
       </body>
     </html>
